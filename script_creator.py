ccf808e454498e45fd600d9dd8441410ecaf31d2
Afajobi <105067655+Acerock1@users.noreply.github.com>
2023-05-08 11:28:38 -0500

    Update script_creator.py
    testing script

--- script_creator.py
+++ script_creator.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python3
 
+import requests
 import os
-import argparse
-from langchain import 
+import openai
+ 
+
+Openai_prompt = input("How may I help you today?")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Completion.create(
+  model="text-davinci-003",
+  prompt="f"{Openai_prompt}",
+  max_tokens=7,
+  temperature=0.5
+)
+
+print(openai.completion.create)
+
+
+'''
+api_endpoint =  https://api.openai.com/v1/completions
+api_key = ""
+
+openai.Completion.create(
+  model="text-davinci-003",
+  prompt='f"Write a "',
+  max_tokens=100,
+  temperature=
+)
+ 
+'''
