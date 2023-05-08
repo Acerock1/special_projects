81c63106bd74de2fec58145c3261b773f2af246a
Afajobi <105067655+Acerock1@users.noreply.github.com>
2023-05-08 12:12:49 -0500

    Update script_creator.py

--- script_creator.py
+++ script_creator.py
@@ -6,10 +6,10 @@ import openai
  
 
 Openai_prompt = input("How may I help you today?")
-openai.api_key = 
-openai.Completion.create(model = "text-davinci-003" ,prompt = Openai_prompt ,max_tokens = 7 ,temperature = 0.5)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+response = openai.Completion.create(model = "text-davinci-003" ,prompt = Openai_prompt ,max_tokens = 7 ,temperature = 0.5)
   
 
-print(openai.completion.create)
+print(response.json)
 
 
