5eb30558c36612d53f8d3809070c9cd9ba466395
Acerock1 <afolabi.fajobi@lions.lincoln.edu>
2023-05-08 17:05:42 +0000

    4

--- script_creator.py
+++ script_creator.py
@@ -6,7 +6,7 @@ import openai
  
 
 Openai_prompt = input("How may I help you today?")
-openai.api_key = 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
+openai.api_key = 
 openai.Completion.create(model = "text-davinci-003" ,prompt = Openai_prompt ,max_tokens = 7 ,temperature = 0.5)
   
 
