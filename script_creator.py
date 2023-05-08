6405d7ffcf9c3f0ca4ea66234553127d6a971521
Afajobi <105067655+Acerock1@users.noreply.github.com>
2023-05-08 13:14:55 -0500

    Update script_creator.py

--- script_creator.py
+++ script_creator.py
@@ -5,15 +5,19 @@ import os
 import openai
 import argparse
 
-ai_key = ""
-Openai_prompt = input("How may I help you today?")
+'''
+parser = argparse.ArgumentParser(description='OpenAI prompt')
+parser.add_argument('prompt', metavar='P', type=str, help='The prompt for the OpenAI API')
+args = parser.parse_args()
+'''
+ai_key = "YOUR_OPENAI_API_KEY"
 openai.api_key = ai_key
 response = openai.Completion.create(
-        model = "text-davinci-003" ,
-        prompt = Openai_prompt ,
-        max_tokens = 7 ,
-        temperature = 0.5)
-  
+    model="text-davinci-003",
+    prompt=args.prompt,
+    max_tokens=7,
+    temperature=0.5
+)
 
 print(response)
 
