e92ec1371c9c82633e7361cc27f5587b14c85122
Acerock1 <afolabi.fajobi@lions.lincoln.edu>
2023-05-08 17:59:16 +0000

    commit

--- script_creator.py
+++ script_creator.py
@@ -5,11 +5,16 @@ import os
 import openai
 import argparse
 
+ai_key = "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"
 Openai_prompt = input("How may I help you today?")
-openai.api_key = os.getenv("OPENAI_API_KEY")
-response = openai.Completion.create(model = "text-davinci-003" ,prompt = Openai_prompt ,max_tokens = 7 ,temperature = 0.5)
+openai.api_key = ai_key
+response = openai.Completion.create(
+        model = "text-davinci-003" ,
+        prompt = Openai_prompt ,
+        max_tokens = 7 ,
+        temperature = 0.5)
   
 
-print(response.json)
+print(response)
 
 
