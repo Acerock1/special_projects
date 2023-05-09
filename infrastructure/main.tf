443405b330d7e746a6a87bb2da7a6c7cc37a8d75
Afajobi <105067655+Acerock1@users.noreply.github.com>
2023-05-09 05:23:04 +0000

    updates

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,22 @@
 terraform {
   required_providers {
     aws = {
-        
+        source = "harshicorp.aws"
+        version = "4.0"
     }
   }
+}
+
+resource "aws_vpc" "Project_vpc" {
+  cidr_block = "123.0.0.0/16"
+
+  tags = {
+    name = "project_vpc"
+  }
+  
+}
+
+resource "aws_subnet" "Private_subnet1" {
+  vpc_id = ''
+  
 }
